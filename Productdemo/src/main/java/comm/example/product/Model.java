7280dde9189ed79ccfbe817f5e33fcfb65d26d7a
Alchemy <Alchemy@192.168.1.155>
2020-01-27 04:34:38 -0800

    producthtmldemo

---
+++ Productdemo/src/main/java/comm/example/product/Model.java
@@ -0,0 +1,22 @@
+package comm.example.product;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+
+public class Model {
+	private String productname;
+	private String productid;
+	private String description;
+
+}
