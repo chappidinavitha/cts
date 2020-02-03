ef88a17d94512e5cf4f9c4c43841f987921a82b0
Alchemy <Alchemy@192.168.1.155>
2020-02-03 03:35:28 -0800

    springbootapp

---
+++ StudentSpringBoot/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Generated;
+
+import lombok.Data;
+
+@Entity
+@Data
+
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
+}
